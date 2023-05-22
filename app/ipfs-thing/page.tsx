d4791b9b3a56e9d1353bf9707ac72c5f1649193b
Anastasiya Uraleva <anastasiyauraleva@Anastasiyas-MacBook-Pro.local>
2023-05-22 15:58:31 -0400

    testing google calendar embed

--- app/ipfs-thing/page.tsx
+++ app/ipfs-thing/page.tsx
@@ -7,7 +7,7 @@ import DefaultLayout from '@components/DefaultLayout';
 import Footer from '@root/components/Footer';
 import GutterContainer from '@root/components/GutterContainer';
 import ResponsiveNabar from '@root/components/ResponsiveNavbar';
-import SectionHomepage from '@root/components/SectionHomepage';
+import SectionEventPage from '@root/components/SectionHomepage';
 
 export default async function Page(props) {
   const hero = HOMEPAGE_HERO_CONTENT;
@@ -18,7 +18,7 @@ export default async function Page(props) {
     <DefaultLayout>
       <ResponsiveNabar navContent={navContent} />
       <GutterContainer>
-        <SectionHomepage hero={hero} />
+        <SectionEventPage hero={hero} />
       </GutterContainer>
 
       <Footer {...footerContent} />
