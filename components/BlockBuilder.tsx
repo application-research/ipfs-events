067ce7e841afc2b0d24f249b403510af87bb3016
Anastasiya Uraleva <anastasiyauraleva@Anastasiyas-MBP.fios-router.home>
2023-07-25 02:21:14 -0400

    updated copy and added cta

--- components/BlockBuilder.tsx
+++ components/BlockBuilder.tsx
@@ -1,14 +1,20 @@
 import styles from '@components/BlockBuilder.module.scss';
 
 import { Block } from './Block';
+import { CallToActionVariant } from './CallToActionVariant';
 
-export default function BlockBuilder({ title, id, border, description, direction, block }) {
+export default function BlockBuilder({ block, cta, title, id, border, description, direction }) {
   return (
     <div id={id} style={{ display: 'grid', rowGap: '2rem' }}>
       {(title || description) && (
         <section style={{ display: 'grid', rowGap: '1rem', borderBottom: border ? '1px solid var(--color-text)' : '' }}>
-          {title && <h1 className={styles.mainTitle}>{title}</h1>}
-          {description && <p>{description}</p>}
+          <div>
+            {' '}
+            {title && <h1 className={styles.mainTitle}>{title}</h1>}
+            {description && <p>{description}</p>}
+          </div>
+
+          {cta && <CallToActionVariant cta={cta} type={cta.type} />}
         </section>
       )}
 
