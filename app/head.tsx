54815c91732cdf7afe3c14529d26c5959490f7ac
Anastasiya Uraleva <anastasiyauraleva@Anastasiyas-MBP.fios-router.home>
2023-07-26 16:07:21 -0400

    left only event page

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,11 @@ import DefaultMetaTags from '@components/DefaultMetaTags';
 import PlausibleScript from '@root/components/PlausibleScript';
 
 export default async function Head({ params }) {
+  const title = 'Filecoin Dev Summit';
+  const description = 'Filecoin Dev Summit 2023 - Singapore and Iceland ';
+  const url = 'https://filecoindevsummit.io';
+
+  // SUMMARY_LARGE_IMAGE: 1500x785
   return (
     <>
       <DefaultMetaTags />
--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,36 @@
 import '@root/global.scss';
 
-import { FOOTER_TINY_CONTENT } from '@root/content/footer-content';
-import { HOMEPAGE_CONTENT } from '@root/content/homepage-content';
-import { NAVIGATION_HOMEPAGE_CONTENT } from '@root/content/navigation-content';
-import DefaultLayout from '@components/DefaultLayout';
 import FooterTiny from '@root/components/FooterTiny';
-import GutterContainer from '@root/components/GutterContainer';
-import NavbarLogoMiddle from '@root/components/NavbarLogoMiddle';
-import SectionHomepage from '@root/components/SectionHomepage';
 import Hero from '@root/components/Hero';
+import ResponsiveNavbar from '@root/components/ResponsiveNavbar';
 import SectionEventPage from '@root/components/SectionEventPage';
-
-export async function generateMetadata({ params, searchParams }) {
-  const title = 'IPFS.Events';
-  const description =
-    "Welcome to IPFS Events: IPFS powers the Distributed Web & we’re powering their events! IPFS is a peer-to-peer hypermedia protocol designed to preserve and grow humanity's knowledge by making the web upgradeable, resilient, and more open.";
-  const url = 'https://ipfsevents.io';
-
-  return {
-    title,
-    description,
-    url,
-    openGraph: {
-      title,
-      description,
-      url,
-      // SUMMARY_LARGE_IMAGE: 1500x785
-      images: [''],
-    },
-    twitter: {
-      title,
-      description,
-      url,
-      handle: '@filecoin',
-      cardType: 'summary_large_image',
-    },
-  };
-}
+import {
+  FILECOIN_DEV_SUMMIT_2023_HERO_CONTENT,
+  FILECOIN_DEV_SUMMIT_2023_PAGE_CONTENT,
+  FILECOIN_DEV_SUMMIT_NAVIGATION_CONTENT,
+  FILECOIN_DEV_SUMMIT_PAGE_STYLE_CONTENT,
+  FOOTER_FILECOIN_DEV_SUMMIT_CONTENT,
+} from '@root/content/filecoin-dev-summit';
 
 export default async function Page(props) {
-  const footerContent = FOOTER_TINY_CONTENT;
-  const blocks = HOMEPAGE_CONTENT;
-  const navContent = NAVIGATION_HOMEPAGE_CONTENT;
+  const blocks = FILECOIN_DEV_SUMMIT_2023_PAGE_CONTENT;
+  const footerContent = FOOTER_FILECOIN_DEV_SUMMIT_CONTENT;
+  const hero = FILECOIN_DEV_SUMMIT_2023_HERO_CONTENT;
+  const navContent = FILECOIN_DEV_SUMMIT_NAVIGATION_CONTENT;
+  const pageStyle = FILECOIN_DEV_SUMMIT_PAGE_STYLE_CONTENT;
 
   return (
-    <DefaultLayout>
-      <NavbarLogoMiddle navContent={navContent} />
+    <div style={{ background: pageStyle.backgroundColor, color: pageStyle.textColor }}>
+      <ResponsiveNavbar navContent={navContent} />
+
+      <div style={{ paddingBottom: '4rem' }}>
+        <Hero {...hero} />
+      </div>
+
+      <SectionEventPage blocks={blocks} pageStyle={pageStyle} />
 
       <SectionEventPage blocks={blocks} />
       <FooterTiny {...footerContent} />
-    </DefaultLayout>
+    </div>
   );
 }
