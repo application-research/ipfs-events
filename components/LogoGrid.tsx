d05dc9fab108e78b9a93cc5fd90d5f3ac914f6c9
Anastasiya Uraleva <anastasiyauraleva@Anastasiyas-MacBook-Pro.local>
2023-08-29 03:51:36 +0900

    updated partner logos

--- components/LogoGrid.tsx
+++ components/LogoGrid.tsx
@@ -7,18 +7,18 @@ export default function LogoGrid({ id, logos, text }) {
   return (
     <div id={id} className={styles.container} style={{ width: '100%' }}>
       {logos && (
-        <div className={styles.col6}>
+        <div className={styles.grid}>
           {logos.map((logo, index) => {
             return (
-              <>
+              <div className={styles.gridItem}>
                 {logo.href !== null ? (
-                  <Link key={index} href={logo.href} target="_blank" className={styles.col}>
+                  <Link key={index} href={logo.href} target="_blank" className={styles.link}>
                     <Image className={styles.logo} src={logo.src} altText={logo?.altText ?? ''} />
                   </Link>
                 ) : (
                   <Image className={styles.logo} src={logo.src} altText={logo?.altText ?? ''} />
                 )}
-              </>
+              </div>
             );
           })}
         </div>
--- pages/api/airtable/iceland.ts
+++ pages/api/airtable/iceland.ts
@@ -6,44 +6,42 @@ import * as Server from '@common/server';
 
 import Airtable from 'airtable';
 
-export default async function apiAirtableIceland(req, res) {
-  await Server.cors(req, res);
-  const name = 'NA/EU Talk Track Submissions + Forms';
-
-  try {
-    const base = new Airtable({ apiKey: process.env.FILSUMMIT_TOKEN }).base(process.env.FILSUMMIT_BASE_ID!);
-    // const records = await base(name).select().firstPage();
-    const records = await base(name).select().all();
-
-    const recordsSize = records.length;
-    res.json({ recordsSize, records });
-  } catch (e) {
-    console.log(e);
-    res.json({ error: true });
-  }
-}
-
 // export default async function apiAirtableIceland(req, res) {
-//   const url = 'https://internet-apis.onrender.com/api/airtable/iceland?version=1';
-//   const TOKEN = process.env.FILSUMMIT_TOKEN;
+//   await Server.cors(req, res);
+//   const name = 'NA/EU Talk Track Submissions + Forms';
 
-//   fetch(url, {
-//     method: 'GET',
-//     headers: {
-//       Authorization: `Bearer ${TOKEN}`,
-//       'Content-Type': 'application/json',
-//     },
-//   })
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error('Network response was not ok');
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       res.json({ data });
-//     })
-//     .catch((error) => {
-//       console.log('There was a problem with the fetch operation:', error.message);
-//     });
+//   try {
+//     const base = new Airtable({ apiKey: process.env.FILSUMMIT_TOKEN }).base(process.env.FILSUMMIT_BASE_ID!);
+//     const records = await base(name).select().firstPage();
+
+// res.json({ records });
+//   } catch (e) {
+//     console.log(e);
+//     res.json({ error: true });
+//   }
 // }
+
+export default async function apiAirtableIceland(req, res) {
+  const url = 'https://internet-apis.onrender.com/api/airtable/iceland?version=1';
+  const TOKEN = process.env.FILSUMMIT_TOKEN;
+
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      res.json({ data });
+    })
+    .catch((error) => {
+      console.log('There was a problem with the fetch operation:', error.message);
+    });
+}
