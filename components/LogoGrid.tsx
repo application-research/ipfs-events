85ca13a6bebdb12f2ad692cf84a5383fb7c09450
Anastasiya Uraleva <anastasiyauraleva@Anastasiyas-MacBook-Pro.local>
2023-08-31 12:49:07 +0900

    added sponsor logo

--- components/LogoGrid.tsx
+++ components/LogoGrid.tsx
@@ -3,12 +3,29 @@ import styles from '@components/LogoGrid.module.scss';
 import Image from './Image';
 import Link from './Link';
 
-export default function LogoGrid({ id, logos, text }) {
+export default function LogoGrid({ id, logosLarge, logosMedium, text }) {
   return (
     <div id={id} className={styles.container} style={{ width: '100%' }}>
-      {logos && (
-        <div className={styles.grid}>
-          {logos.map((logo, index) => {
+      {logosLarge && (
+        <div className={styles.gridLarge}>
+          {logosLarge.map((logo, index) => {
+            return (
+              <div className={styles.gridItem}>
+                {logo.href !== null ? (
+                  <Link key={index} href={logo.href} target="_blank" className={styles.link}>
+                    <Image className={styles.logo} src={logo.src} altText={logo?.altText ?? ''} />
+                  </Link>
+                ) : (
+                  <Image className={styles.logo} src={logo.src} altText={logo?.altText ?? ''} />
+                )}
+              </div>
+            );
+          })}
+        </div>
+      )}
+      {logosMedium && (
+        <div className={`${styles.gridMedium}`}>
+          {logosMedium.map((logo, index) => {
             return (
               <div className={styles.gridItem}>
                 {logo.href !== null ? (
