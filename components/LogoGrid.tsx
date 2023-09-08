f343a6a142af5e74253d91cccaf1248a79c5f64b
Anastasiya Uraleva <anastasiyauraleva@Anastasiyas-MacBook-Pro.local>
2023-09-08 22:36:46 +0900

    added small logo

--- components/LogoGrid.tsx
+++ components/LogoGrid.tsx
@@ -3,7 +3,7 @@ import styles from '@components/LogoGrid.module.scss';
 import Image from './Image';
 import Link from './Link';
 
-export default function LogoGrid({ id, logosLarge, logosMedium, text }) {
+export default function LogoGrid({ id, logosLarge, logosMedium, logosSmall, text }) {
   return (
     <div id={id} className={styles.container} style={{ width: '100%' }}>
       {logosLarge && (
@@ -23,9 +23,28 @@ export default function LogoGrid({ id, logosLarge, logosMedium, text }) {
           })}
         </div>
       )}
+
       {logosMedium && (
         <div className={`${styles.gridMedium}`}>
           {logosMedium.map((logo, index) => {
+            return (
+              <div className={styles.gridItem}>
+                {logo.href !== null ? (
+                  <Link key={index} href={logo.href} target="_blank" className={styles.link}>
+                    <Image className={styles.logo} src={logo.src} altText={logo?.altText ?? ''} />
+                  </Link>
+                ) : (
+                  <Image className={styles.logo} src={logo.src} altText={logo?.altText ?? ''} />
+                )}
+              </div>
+            );
+          })}
+        </div>
+      )}
+
+      {logosSmall && (
+        <div className={`${styles.gridSmall}`}>
+          {logosSmall.map((logo, index) => {
             return (
               <div className={styles.gridItem}>
                 {logo.href !== null ? (
