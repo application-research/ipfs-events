7b930c827c68b4affaa85d8d9743c07d52d93397
nschafer19 <94389054+nschafer19@users.noreply.github.com>
2023-09-08 16:28:11 -0500

    logo update

--- content/filecoin-dev-summit.tsx
+++ content/filecoin-dev-summit.tsx
@@ -129,6 +129,10 @@ export const FILECOIN_DEV_SUMMIT_2023_PAGE_CONTENT = [
             src: '/media/greaterheat.png',
             href: 'https://www.greaterheat.com/',
           },
+           {
+            src: 'https://ibb.co/m0xVz91',
+            href: 'https://glif.io/',
+          },
           {
 <<<<<<< HEAD
 <<<<<<< HEAD
