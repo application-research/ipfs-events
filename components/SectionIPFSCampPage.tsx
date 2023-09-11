47c2edaac7060284d2ab448bbbaaa934c9d39a49
Xiangan He <xiangan9@gmail.com>
2023-09-11 16:49:49 -0400

    fix: ipfs camp logo SVG

--- components/SectionIPFSCampPage.tsx
+++ components/SectionIPFSCampPage.tsx
@@ -57,7 +57,7 @@ export default function SectionCamppage({ upcomingEvents }) {
       <GutterContainer>
         <div style={{ display: 'grid', rowGap: '8rem' }}>
           <section>
-            <img src="/media/ipfs-camp-2024.png" alt="IPFS Camp 2024" className={styles.imageClass} style={{ marginBottom: "2rem" }} />
+            <img src="/media/ipfscamp-2024.svg" alt="IPFS Camp 2024" className={styles.imageClass} style={{ marginBottom: "2rem" }} />
             <header className={styles.header} style={{ paddingBottom: '4rem' }}>
               <div className={styles.headerText}>
                 <h2 className={styles.h2}>IPFS is more than just a ‘conference’; it’s a global movement of builders and visionaries committed to creating a better web. </h2>
