71e0e66a0e8d9660767594d847442ade5926b675
Anastasiya Uraleva <anastasiyauraleva@Anastasiyas-MBP.fios-router.home>
2023-09-26 17:59:25 -0400

    updated to utc standard time

--- resolvers/schedule-talks-resolver.ts
+++ resolvers/schedule-talks-resolver.ts
@@ -1,14 +1,16 @@
 import moment from 'moment';
 
 export const generateHourlyTimeRanges = (times: string[]): string[] => {
+  console.log(times, 'times');
   // Extract unique hours from the provided times
-  const hours: number[] = [...new Set(times.map((time) => moment(time, 'hh:mm a').hour()))].sort((a, b) => a - b);
+  const hours: number[] = [...new Set(times.map((time) => moment.utc(time, 'hh:mm a').hour()))].sort((a, b) => a - b);
 
   // Generate hourly ranges
   const ranges: string[] = [];
   for (let i = 0; i < hours.length - 1; i++) {
     const start = moment.utc({ hour: hours[i] }).format('hh:mm a');
     const end = moment.utc({ hour: hours[i + 1] }).format('hh:mm a');
+
     ranges.push(`${start} - ${end}`);
   }
   return ranges;
@@ -31,7 +33,7 @@ export const extractAllTracksFromTrackDetails = (formattedAirtableData: any): st
 
 export const getTalkWithinSelectedHour = (eventTime: string, selectedTime: string): boolean => {
   const eventMoment = moment.utc(eventTime, 'hh:mm a');
-  const selectedMomentStart = moment.utc(selectedTime, 'HH:mm a');
+  const selectedMomentStart = moment.utc(selectedTime, 'hh:mm a');
   const selectedMomentEnd = selectedMomentStart.clone().add(1, 'hour');
 
   return eventMoment.isSameOrAfter(selectedMomentStart) && eventMoment.isBefore(selectedMomentEnd);
