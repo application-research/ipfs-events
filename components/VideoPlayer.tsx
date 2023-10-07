c9867e6e3bd136d6ae96d9d944ea448832691199
Anastasiya Uraleva <anastasiyauraleva@Anastasiyas-MBP.fios-router.home>
2023-10-06 22:41:50 -0400

    add preview video

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,6 +1,10 @@
 'use client';
 
-import AspectRatio from './AspectRatio';
+import '@vidstack/react/player/styles/default/theme.css';
+import '@vidstack/react/player/styles/default/layouts/video.css';
+import { MediaPlayer, MediaProvider } from '@vidstack/react';
+import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/layouts/default';
+// import AspectRatio from './AspectRatio';
 
 export function VideoPlayer({ props, ariaLabel, autoplay, controls, loop, muted, ratio, src }: any) {
   const videoProps = {
@@ -8,10 +12,19 @@ export function VideoPlayer({ props, ariaLabel, autoplay, controls, loop, muted,
     autoPlay: autoplay,
     muted: muted,
   };
-
+  //autoplay={videoProps.autoPlay} muted={videoProps.muted} loop={loop ?? true}
   return (
-    <AspectRatio ratio={ratio} props={{ background: 'var(--color-black)', borderRadius: 'var(--border-radius-small)' }}>
-      <video
+    <div>
+      <MediaPlayer title={ariaLabel} src={src}>
+        <MediaProvider />
+        <DefaultVideoLayout thumbnails={src} icons={defaultLayoutIcons} />
+      </MediaPlayer>
+    </div>
+  );
+}
+// <AspectRatio ratio={ratio} props={{ background: 'var(--color-black)', borderRadius: 'var(--border-radius-small)' }}>
+{
+  /* <video
         style={{ borderRadius: 'var(--border-radius-small)', overflow: 'hidden' }}
         {...videoProps}
         autoPlay
@@ -24,7 +37,5 @@ export function VideoPlayer({ props, ariaLabel, autoplay, controls, loop, muted,
       >
         <source src={src} type="video/mp4" />
         Your browser does not support the video tag.
-      </video>
-    </AspectRatio>
-  );
-}
+      </video> */
+} // </AspectRatio>
