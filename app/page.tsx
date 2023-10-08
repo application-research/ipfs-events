15767c7d7c7a6c371016a0d0758a208f0e8a909c
Xiangan He <xiangan9@gmail.com>
2023-10-08 11:03:24 -0400

    fastload feat

--- app/page.tsx
+++ app/page.tsx
@@ -50,13 +50,8 @@ export default async function Page(props) {
   const promises = blocks.flatMap(contentItem => 
     contentItem.block.map(async blockItem => {
       if ('scheduleData' in blockItem && blockItem.scheduleData.airtable) {
-        try {
-          const data = await makeRequest({ endpoint: blockItem.scheduleData.airtable.endPoint });
-          console.log(blockItem.scheduleData);
-          blockItem.scheduleData.airtable.data = data;
-        } catch (error) {
-          console.error('Error fetching tableData for blockItem:', blockItem, error);
-        }
+        const data = await makeRequest({ endpoint: blockItem.scheduleData.airtable.endPoint });
+        blockItem.scheduleData.airtable.data = data;
       }
     })
   );
