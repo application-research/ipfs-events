ca8f3eae5a617533a78f5b9799b8284521b490d7
xBalbinus <xiangan9@gmail.com>
2024-02-07 13:08:08 -0500

    feat: redirects on iceland / singapore lists

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/iceland-list',
+        destination: '/FDS-1/iceland-list',
+        permanent: true,
+      },
+      {
+        source: '/singapore-list',
+        destination: '/FDS-2/singapores-list',
+        permanent: true,
+      },
+    ]
+  }
 };
 
 module.exports = nextConfig;
